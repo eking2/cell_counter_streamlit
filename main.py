011024d96e88b6e4c4d5624445c28a0f7e7b6f0e
eking2 <eking2@uci.edu>
2020-09-16 02:00:54 -0700

    pixel intensity hist and thresholding

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import bokeh
 from bokeh.plotting import figure
+from bokeh.models import WheelZoomTool, Span
 import streamlit as st
 import skimage.morphology
 from skimage.color import rgb2gray
 import skimage.filters
 import skimage.io
+import skimage.restoration
+import skimage.segmentation
 import numpy as np
 
 # disable fileuploader deprecation warn
@@ -23,6 +26,8 @@ cmap_dict = {'Greys' : bokeh.palettes.grey(256),
 
 def bokeh_imshow(img, height=800, cmap=None):
 
+    '''plot image array with bokeh'''
+
     # dims and color
     n, m = img.shape
     c = bokeh.models.LinearColorMapper(cmap_dict[cmap])
@@ -32,6 +37,9 @@ def bokeh_imshow(img, height=800, cmap=None):
     p = figure(plot_height=height, plot_width=width,
                x_range=[0, m], y_range=[0, n])
     p.image([np.flipud(img)], x=0, y=0, dw=m, dh=n, color_mapper=c)
+
+    # wheel zoom on
+    p.toolbar.active_scroll = p.select_one(WheelZoomTool)
 
     return p
 
@@ -53,25 +61,100 @@ def filter_img(img, median, gauss, chambolle):
         img_float = skimage.img_as_float(img)
         img = img_float - img_gauss
 
+    # total variation filter, denoise
+    if chambolle > 0:
+        img = skimage.restoration.denoise_tv_chambolle(img, weight=chambolle)
+
+    # [0, 1] to [0, 255]
+    if img.max() <= 1.0:
+        img_min = img.min()
+        img_max = img.max()
+
+        x_std = (img - img_min) / (img_max - img_min)
+        img = x_std * (255 - 0) + 0
+
     return img
+
+
+def img_hist(img, height, thresh=None):
+
+    '''plot histogram of image intensity values'''
+
+    img_arr = img.flatten() 
+
+    # freedman-diaconis to select n bins
+    #q75, q25 = np.percentile(img_arr, [75, 25])
+    #denom = len(img_arr)**(1/3)
+    #bin_width = 2 * (q75 - q25)/denom
+    #bins = (img_arr.max() - img_arr.min()) / bin_width
+    #bins = int(bints)
+
+    hist, edges = np.histogram(img_arr, 'auto', density=True)
+
+    # plot
+    p = figure(plot_height=height-100)
+    p.quad(top=hist, bottom=0, left=edges[:-1], right=edges[1:])
+    p.toolbar.active_scroll = p.select_one(WheelZoomTool)
+    p.xaxis.axis_label = 'Pixel intensity'
+    p.yaxis.axis_label = 'Density'
+
+    if thresh is not None:
+        thresh_line = Span(location=thresh, dimension='height', line_color='red', line_width=2)
+        p.add_layout(thresh_line)
+
+    return p
+
+def segment(img, thresh, border, area_bounds, ecc_bounds):
+
+    '''segment image by threshold and count regions'''
+
+    img_thresh = img < thresh
+
+    # label each segment, first round without border, size, area cutoffs
+    img_labels = skimage.measure.label(img_thresh)
+
+    # ignore cells that are on border
+    img_thresh = skimage.segmentation.clear_border(img_labels, buffer_size=border)
+
+    # cutoff on cell size
+    # and eccentricity (how circular, 0 = perfect circle, 1 = ellipse)
+    props = skimage.measure.regionprops(img_thresh) 
+    areas = [prop.area for prop in props]
+    print(areas)
+
+    
+
+    import matplotlib.pyplot as plt
+
+    plt.imshow(img_thresh)
+    st.pyplot()
+
 
 
 # sidebar options
 st.sidebar.markdown('## Image options')
 cmap = st.sidebar.selectbox('Colormap', ['Greys', 'Inferno', 'Magma', 'Plasma', 'Viridis', 'Cividis', 'Turbo'], 4)
+height = st.sidebar.number_input('Height', min_value=200, value=800)
 
 st.sidebar.markdown('## Filters')
 median = st.sidebar.slider('Median filter', min_value=0, max_value=10, value=0, step=1)
 gauss = st.sidebar.slider('Background subtraction (Gaussian)', min_value=0, max_value=100, value=0, step=5)
-chambolle = st.sidebar.slider('Denoising variation filter (Chambolle)', min_value=0.0, max_value=0.1, step=0.01)
+chambolle = st.sidebar.slider('Denoising variation filter (Chambolle)', min_value=0.0, max_value=0.5, step=0.05)
 
 st.sidebar.markdown('## Thresholding')
+thresh_method = st.sidebar.radio('Method', ['Manual', 'Otsu'])
+if thresh_method == 'Manual':
+    thresh = st.sidebar.slider('Pixel threshold', min_value=0, max_value=256, value=0, step=1)
+border = st.sidebar.slider('Border pixel buffer', min_value=0, max_value=10, value=0, step=1)
+area = st.sidebar.slider('Area bounds', min_value=0, max_value=100, value=(0, 100), step=1)
+ecc = st.sidebar.slider('Eccentricity bounds', min_value=0.0, max_value=1.0, value=(0.8, 1.0), step=0.1)
 
 
 st.sidebar.markdown('## Mask overlay')
 mask = st.sidebar.radio('Segments', ['None', 'Unique', 'Full'])
 
 # load image
+st.title('Cell counter')
 img_f = st.file_uploader('Input image', type=['jpg', 'tif', 'png'])
 if img_f is not None:
 
@@ -82,7 +165,21 @@ if img_f is not None:
 
     # filter
     img = filter_img(img, median, gauss, chambolle)
-    p = bokeh_imshow(img, cmap=cmap)
+    p = bokeh_imshow(img, height, cmap)
     st.bokeh_chart(p)
+    st.text('')
+
+    # otsu on filtered image
+    if thresh_method == 'Otsu':
+        thresh = skimage.filters.threshold_otsu(img)
+
+    # get mask for cells
+
+    # plot intensity hist
+    p2 = img_hist(img, height, thresh)
+    st.bokeh_chart(p2)
+    
+    segment(img, thresh, border, area, ecc)
+
 
 
